9420adb5bd64e00e88a95a2d7ade9d1e45accd88
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2022-12-23 17:43:17 +0900

    Add files via upload

---
+++ turtle/turtle_1.py
@@ -0,0 +1,4 @@
+a=int(input("첫번째 숫자를 입력하세요."))
+b=int(input("두번째 숫자를 입력하세요."))
+print(a,b)
+print(a+b)
---
+++ turtle/turtle_6.py
@@ -0,0 +1,9 @@
+import turtle
+t=turtle.Turtle()
+t.shape('turtle')
+r=50
+t.circle(r)
+t.goto(100,0)
+t.circle(r+30)
+t.goto(200,0)
+t.circle(r+60)
