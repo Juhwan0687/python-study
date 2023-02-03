328c7e93c01f46b27f2a99c1221e2415f1e0ef40
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-02-03 15:48:49 +0900

    Add files via upload

--- turtle/문자열 슬라이싱.py
+++ turtle/문자열 슬라이싱.py
@@ -1,14 +1,4 @@
-print('*'*10)
-
-print(' '*2+'*')
-print(' '+'*'*3)
-print('*'*5)
-print(' '+'*'*3)
-print(' '*2+'*')
-
-a='Carpe Diem!'
-print(a[0:5])
-
-a=[1,2,3,4,5]
-a[3]='4'
-print(a)
+a=["C","a","r","p","e"," ","D","i","e","m"]
+b=int(input())
+c=int(input())
+print(a[b:c])
