86a97108e5baf16aec00c577e71b6d7ccfa59b87
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-02-10 16:37:08 +0900

    Add files via upload

---
+++ turtle/while2.py
@@ -0,0 +1,9 @@
+sum=0
+b=int(input('정수를 입력하세요.'))
+i=1
+while i<=b:
+    sum+=i
+    i+=1
+print('\n')
+print(sum)
+print('\n')
---
+++ turtle/while4.py
@@ -0,0 +1,9 @@
+i=1
+sum=0
+a=int(input('정수를 입력하세요.'))
+while i<=a:
+    sum+=i
+    i+=1
+print('\n')
+print(sum)
+print('\n')
